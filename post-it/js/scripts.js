b45f25b1c9949a8cf835097446c5e8faed53c181
Caio Costa <caio.costa93@gmail.com>
2018-01-30 21:43:13 -0200

    code refactor

--- post-it/js/scripts.js
+++ post-it/js/scripts.js
@@ -1,8 +1,5 @@
-var notas = [];
-
-var section = Document.getElementById('');
-
 var nota = {
+    secao: document.getElementsByClassName('notes')[0],
     listaInterna: [],
 
     adiciona: function(novoTitulo, novoTexto){
@@ -13,80 +10,72 @@ var nota = {
         };
 
         this.listaInterna.push(nota);
+        
+        atualizarSecao(this.secao);
     },
     remove: function(posicao){
         this.listaInterna.splice(posicao, 1);
+
+        atualizarSecao(this.secao);
     },
     edita: function(posicao){
         this.listaInterna[posicao].editando = true;
+
+        atualizarSecao(this.secao);
     },
     salva: function(posicao, novoTitulo, novoTexto){
         this.listaInterna[posicao].titulo = novoTitulo,
         this.listaInterna[posicao].texto = novoTexto,
         this.listaInterna[posicao].editando = false;
+
+        atualizarSecao(this.secao);
     }
-}
+};
 
 
 function atualizarSecao(secao) {
     var conteudoSecao = "";
 
-    for (var posicao = 0; posicao < notas.length; posicao++) {
-        if (notas[posicao].editando) {
-            //criar variavel para guardar o html de todas as notas que estão aparecendo na tela
+    for (var posicao = 0; posicao < nota.listaInterna.length; posicao++) {
+        if (nota.listaInterna[posicao].editando) {
+            //criar variavel para guardar o html de todas as nota.listaInterna que estão aparecendo na tela
             conteudoSecao += '<form class="note">' +
-                '<input class="note__title" type="text" name="titulo" value="' + notas[posicao].titulo + '" placeholder="Título">' +
-                '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + notas[posicao].texto + '</textarea>' +
-                '<button class="note__control" type="button" onclick="adicionarNota(this.form.titulo, this.form.texto, this.form, this.form.parentElement, ' + posicao + ')">' +
+                '<input class="note__title" type="text" name="titulo" value="' + nota.listaInterna[posicao].titulo + '" placeholder="Título">' +
+                '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + nota.listaInterna[posicao].texto + '</textarea>' +
+                '<button class="note__control" type="button" onclick="adicionarNota(this.form.titulo, this.form.texto, this.form, ' + posicao + ')">' +
                 'Concluído' +
                 '</button>' +
                 '</form>';
         } else {
             conteudoSecao += '<form class="note" onclick="editarFormulario(' + posicao + ', this.parentElement)">' +
-                '<button class="note__control" type="button" onclick="removerNota(event, ' + posicao + ', this.form.parentElement)">' +
+                '<button class="note__control" type="button" onclick="removerNota(event, ' + posicao + ')">' +
                 '<i class="fa fa-times" aria-hidden="true"></i>' +
                 '</button>' +
-                '<h1 class="note__title">' + notas[posicao].titulo + '</h1>' +
-                '<p class="note__body">' + notas[posicao].texto + '</p>' +
+                '<h1 class="note__title">' + nota.listaInterna[posicao].titulo + '</h1>' +
+                '<p class="note__body">' + nota.listaInterna[posicao].texto + '</p>' +
                 '</form>';
         }
     }
     secao.innerHTML = conteudoSecao;
 }
 
-function editarFormulario(posicao, secao) {
-    notas[posicao].editando = true;
-
-    // atualizarSecao(secao);
+function editarFormulario(posicao) {
+    nota.edita(posicao);    
 }
 
-function adicionarNota(inputTitulo, textareaTexto, formulario, secao, posicao) {
-    if (notas[posicao]) {
-        notas[posicao].titulo = inputTitulo.value,
-        notas[posicao].texto = textareaTexto.value,
-        notas[posicao].editando = false;
-        
-        // atualizarSecao(secao);
+function adicionarNota(InputTitulo, InputareTexto, formulario, posicao) {
+    if (nota.listaInterna[posicao]) {
+        nota.salva(posicao, InputTitulo.value, InputareTexto.value);
 
     } else {
-        var nota = {
-            titulo: inputTitulo.value,
-            texto: textareaTexto.value,
-            editando: false
-        }
-
-        notas.push(nota);
-
-        // atualizarSecao(secao);
+        nota.adiciona(InputTitulo.value, InputareTexto.value);
 
         formulario.reset();
     }
 }
 
-function removerNota(event, posicao, secao) {
+function removerNota(posicao) {
     event.stopPropagation();
 
-    notas.splice(posicao, 1);
-
-    // atualizarSecao(secao);
+    nota.remove(posicao);
 }
