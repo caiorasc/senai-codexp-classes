2956cfa018a6124774062240a816e3b74db71168
Caio Costa <caio.costa93@gmail.com>
2018-02-01 20:08:43 -0200

    Refactor script.js to use es6 methods and add watch on babel

--- post-it-ES6/lib/scripts.js
+++ post-it-ES6/lib/scripts.js
@@ -1,75 +1,68 @@
 "use strict";
 
-var nota = {
-    secao: document.getElementsByClassName('notes')[0],
+var listaNotas = {
+    secao: document.getElementsByClassName("notes")[0],
     listaInterna: [],
-
     adiciona: function adiciona(novoTitulo, novoTexto) {
         var nota = {
             titulo: novoTitulo,
             texto: novoTexto,
             editando: false
         };
-
-        undefined.listaInterna.push(nota);
-
-        atualizarSecao(undefined.secao);
+        this.listaInterna.push(nota);
+        atualizarSecao(this.secao);
     },
     remove: function remove(posicao) {
-        undefined.listaInterna.splice(posicao, 1);
-
-        atualizarSecao(undefined.secao);
+        this.listaInterna.splice(posicao, 1);
+        atualizarSecao(this.secao);
     },
     edita: function edita(posicao) {
-        undefined.listaInterna[posicao].editando = true;
-
-        atualizarSecao(undefined.secao);
+        this.listaInterna[posicao].editando = true;
+        atualizarSecao(this.secao);
     },
     salva: function salva(posicao, novoTitulo, novoTexto) {
-        undefined.listaInterna[posicao].titulo = novoTitulo, undefined.listaInterna[posicao].texto = novoTexto, undefined.listaInterna[posicao].editando = false;
-
-        atualizarSecao(undefined.secao);
+        this.listaInterna[posicao].titulo = novoTitulo;
+        this.listaInterna[posicao].texto = novoTexto;
+        this.listaInterna[posicao].editando = false;
+        atualizarSecao(this.secao);
     },
     pega: function pega(posicao) {
-        return undefined.listaInterna[posicao];
+        return this.listaInterna[posicao]; //Não esquecer do return quando migrar o arrow function para método
     },
     contaTotal: function contaTotal() {
-        return undefined.listaInterna.length;
+        return this.listaInterna.length; //Não esquecer do return quando migrar o arrow function para método
     }
 };
 
-function atualizarSecao(secao) {
+var atualizarSecao = function atualizarSecao(secao) {
     var conteudoSecao = "";
 
-    for (var posicao = 0; posicao < nota.contaTotal(); posicao++) {
-        var notaAtual = nota.pega(posicao);
-
+    for (var posicao = 0; posicao < listaNotas.contaTotal(); posicao++) {
+        var notaAtual = listaNotas.pega(posicao);
         if (notaAtual.editando) {
-            //criar variavel para guardar o html de todas as nota.listaInterna que estão aparecendo na tela
-            conteudoSecao += "<form class=\"note\">\n                <input class=\"note__title\" type=\"text\" name=\"titulo\" value=\" " + notaAtual.titulo + " \" placeholder=\"T\xEDtulo\">\n                <textarea class=\"note__body\" name=\"texto\" rows=\"5\" placeholder=\"Criar uma nota...\">" + notaAtual.texto + "</textarea>\n                <button class=\"note__control\" type=\"button\" onclick=\"adicionarNota(this.form.titulo, this.form.texto, this.form, " + posicao + ")\">\n                Conclu\xEDdo\n                </button>\n                </form>";
+            conteudoSecao += "<form class=\"note\">\n                                <input class=\"note__title\" type=\"text\" name=\"titulo\" value=\"" + notaAtual.titulo + "\" placeholder=\"T\xEDtulo\">\n                                <textarea class=\"note__body\" name=\"texto\" rows=\"5\" placeholder=\"Criar uma nota...\">" + notaAtual.texto + "</textarea>\n                                <button class=\"note__control\" type=\"button\" onclick=\"adicionarNota(this.form.titulo, this.form.texto, this.form, " + posicao + ")\">\n                                    Conclu\xEDdo\n                                </button>\n                              </form>";
         } else {
-            conteudoSecao += "<form class=\"note\" onclick=\"editarFormulario( " + posicao + ", this.parentElement)\">\n                <button class=\"note__control\" type=\"button\" onclick=\"removerNota(event, " + posicao + ")\">\n                <i class=\"fa fa-times\" aria-hidden=\"true\"></i>\n                </button>\n                <h1 class=\"note__title\"> " + notaAtual.titulo + "</h1>\n                <p class=\"note__body\"> " + notaAtual.texto + " </p>\n                </form>";
+            conteudoSecao += "<form class=\"note\" onclick=\"editaFormulario(" + posicao + ")\">\n                                <button class=\"note__control\" type=\"button\" onclick=\"removerNota(event, " + posicao + ")\">\n                                    <i class=\"fa fa-times\" aria-hidden=\"true\"></i>\n                                </button>\n                                <h1 class=\"note__title\">" + notaAtual.titulo + "</h1>\n                                <p class=\"note__body\">'" + notaAtual.texto + "</p>\n                              </form>";
         }
     }
+
     secao.innerHTML = conteudoSecao;
-}
+};
 
-function editarFormulario(posicao) {
-    nota.edita(posicao);
-}
+var editaFormulario = function editaFormulario(posicao) {
+    return listaNotas.edita(posicao);
+};
 
-function adicionarNota(InputTitulo, InputareTexto, formulario, posicao) {
-    if (nota.pega(posicao)) {
-        nota.salva(posicao, InputTitulo.value, InputareTexto.value);
+var adicionarNota = function adicionarNota(inputTitulo, textareaTexto, formulario, posicao) {
+    if (listaNotas.pega(posicao)) {
+        listaNotas.salva(posicao, inputTitulo.value, textareaTexto.value);
     } else {
-        nota.adiciona(InputTitulo.value, InputareTexto.value);
-
+        listaNotas.adiciona(inputTitulo.value, textareaTexto.value);
         formulario.reset();
     }
-}
+};
 
-function removerNota(posicao) {
-    event.stopPropagation();
-
-    nota.remove(posicao);
-}
+var removerNota = function removerNota(evento, posicao) {
+    evento.stopPropagation();
+    listaNotas.remove(posicao);
+};
