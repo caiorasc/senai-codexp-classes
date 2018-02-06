04dc3336720b2d7f468b377e9caa9e44d697cbdd
Caio Costa <caio.costa93@gmail.com>
2018-02-06 21:22:24 -0200

    start to use webpack

---
+++ post-it-ES6/src/listaNotas.js
@@ -0,0 +1,44 @@
+import Nota from './nota.js'
+
+class ListaNotas {
+    constructor(observador) {
+        this._observador = observador;
+        this._listaInterna = [];
+    }
+     adiciona(novoTitulo, novoTexto) {
+        let nota = new Nota(novoTitulo, novoTexto);
+        this._listaInterna.push(nota);
+        this._observador();
+     }
+ 
+     remove(posicao) {
+        this._listaInterna.splice(posicao, 1);
+        this._observador();
+     }
+ 
+     edita(posicao) {
+        
+        this._listaInterna[posicao].editando = true;
+        this._observador();
+     }
+ 
+     salva(posicao, novoTitulo, novoTexto) {
+        
+        this._listaInterna[posicao].titulo = novoTitulo;
+        this._listaInterna[posicao].texto = novoTexto;
+        this._listaInterna[posicao].editando = false;
+        this._observador();
+     }
+ 
+     pega(posicao) {
+        
+        return this._listaInterna[posicao];
+     }
+ 
+     contaTotal() {
+        
+        return this._listaInterna.length;
+     }
+ };
+
+ export default ListaNotas;
